aded6b53a3fa2f8877d7e9e3dff9dcdada9bd1f6
Timothee Grollier <timotheegrollier@gmail.com>
2023-06-17 19:30:41 +0200

    Improve style

--- gpt/views.py
+++ gpt/views.py
@@ -3,11 +3,10 @@ from gpt4free import you
 
 
 def get_gpt_response(answer):
+    prompt = "Tu es une IA spécialisée en programmation et tu parles bien le français. tu es ici pour nous aider avec nos questions et problèmes liés au code. Que ce soit pour discuter des langages de programmation, des concepts d'algorithmes, des bonnes pratiques de développement ou même pour nous aider à résoudre des bugs"
     # simple request with links and details
-    preAnswer = "voici ma question:"
     if(answer != ""):
-        answer = preAnswer + answer
-    prompt = f"parles en francais {answer}"
+        prompt = answer
 
         
     response = you.Completion.create(
@@ -22,8 +21,8 @@ def home(request):
         answer = request.POST.get('prompt')
         response = get_gpt_response(answer)
         context = {'response': response}
-        return render(request, 'gpt/home.html', context)
+        return render(request, 'gpt/_home.html', context)
     else:
         response = get_gpt_response("")
         context = {'response': response}
-        return render(request, 'gpt/home.html',context)
+        return render(request, 'gpt/_home.html',context)
